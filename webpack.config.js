8e78c3a195527bc9c8e749f362481b5ee0c8dbdc
Iwan Ruslani <iruslani75@gmail.com>
2016-02-13 23:18:45 -0800

    Added CSS loaders for web pack and added repository in package.json file.

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,11 @@ module.exports = {
           query: {
             presets: ['es2015','react']
           }
-        }        
+        },
+        {
+          test: /\.css$/,
+          loader: 'style!css'
+        }
       ]
     }
 };
